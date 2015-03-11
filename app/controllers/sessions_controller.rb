262b0b99d550fcfebcf87e8774684cf62a62bf31
Matt Huhman <huhmanbeing@gmail.com>
2015-03-11 14:26:50 -0400

    another damn binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < Devise::SessionsController
 
   def create
     self.resource = warden.authenticate(auth_options)
-    binding.pry
     if self.resource
       sign_in(resource_name, resource)
       render json: {:user => current_user}, status: :ok
