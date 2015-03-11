eb38f64d22430bfc0a6de60d74f8e3ca6e26a2ec
Matt Huhman <huhmanbeing@gmail.com>
2015-03-11 11:40:49 -0400

    Added basic show/index methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UserController < ApplicationController
   before_action :authenticate_user_from_token!
+
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render json: (:user => @user), status: :ok
+    else
+      render json: (:error => @user.error.full_messages), status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @users = User.all
+    if @users
+      render json: (:users => @users), status: :ok
+    else
+      render json: (:error => @users.error.full_messages), status: :unprocessable_entity
+    end
+  end
+
   
 end
