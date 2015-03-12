28818cb0b1ae501e596131ea2fabc4ff77ead588
Matt Huhman <huhmanbeing@gmail.com>
2015-03-12 15:20:41 -0400

    Updated rating validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,8 @@ class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :route
 
+  validates_uniqueness_of :user_id, scope: :route_id
+
 
   def add_to_user_rating_count(user)
     user.stat_tracker.rating_total += 1
